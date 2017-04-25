cc7e190fbc396008d3dc01417d54e21ff81d64ee
helloxen35 <007vijeth@gmail.com>
2017-04-25 15:55:25 -0400

    adding helloworld2 program

---
+++ src/helloworld2/Helloclass.java
@@ -0,0 +1,13 @@
+package helloworld2;
+
+public class Helloclass {
+	
+	public static void Main(String []args)
+	{
+		for(int i=0; i<10 ;i++)
+		{
+			System.out.println("the number is" +i);
+		}
+	}
+
+}
